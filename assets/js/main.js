d93a4472985854a0619138106ba6d36e550c9081
PaoloMarzocchi <marzocchi.3p@gmail.com>
2024-03-13 17:12:52 +0100

    add todo list dynamically

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,28 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-
+            todolist: [
+                {
+                    text: 'Fare la spesa',
+                    done: false,
+                },
+                {
+                    text: 'Portare il cane',
+                    done: true,
+                },
+                {
+                    text: 'Lavare i piatti',
+                    done: false,
+                },
+                {
+                    text: 'Cucinare',
+                    done: true,
+                },
+                {
+                    text: 'Pulire casa',
+                    done: true,
+                },
+            ]
         }
     }
 }).mount('#app')
